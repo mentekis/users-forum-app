4a08d5edec050215196ed713f63ad704b10ab106
Dadit <dadit.utama@gmail.com>
2024-08-17 14:58:48 +0700

    fix: URI

--- src/db-connect/db.connect.ts
+++ src/db-connect/db.connect.ts
@@ -5,9 +5,7 @@ dotenv.config();
 
 export const connectDb = async () => {
   await mongoose
-    .connect(process.env.MONGO_URI as string, {
-      authSource: "admin",
-    })
+    .connect(process.env.MONGO_URI as string)
     .then(() => console.log("MongoDB succesfully connected"))
     .catch((err) => console.log("MongoDB connection failed", err));
 };
