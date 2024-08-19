9c8dc9dd04096d74f3e1ec0d71fa2c065cda87c2
Dadit <dadit.utama@gmail.com>
2024-08-19 22:06:31 +0700

    fix: auth with envalid

--- docker-entrypoint-initdb.d/mongo-init.js
+++ docker-entrypoint-initdb.d/mongo-init.js
@@ -1,16 +1,18 @@
+import { env } from "../src/utils/envalid/env";
+
 print("Starting...");
 
 db_prod = db.getSiblingDB("users_prod");
 db_prod.createUser({
-  user: "forum_users",
-  pwd: "forumAppUsers001",
+  user: env.MONGO_INITDB_ROOT_USERNAME,
+  pwd: env.MONGO_INITDB_ROOT_PASSWORD,
   roles: [{ role: "readWrite", db: "users_prod" }],
 });
 
 db_dev = db.getSiblingDB("users_dev");
 db_dev.createUser({
-  user: "forum_users",
-  pwd: "forumAppUsers001",
+  user: env.MONGO_INITDB_ROOT_USERNAME,
+  pwd: env.MONGO_INITDB_ROOT_PASSWORD,
   roles: [{ role: "readWrite", db: "users_dev" }],
 });
 
