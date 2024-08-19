768cd5633b7e0b6aad28baf61e8a80dfea69c3b5
Dadit <dadit.utama@gmail.com>
2024-08-19 19:54:35 +0700

    fix: reset env follow envalid

--- src/utils/db-connect/db.connect.ts
+++ src/utils/db-connect/db.connect.ts
@@ -1,11 +1,9 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
+import { env } from "../envalid/env";
 
 export const connectDb = async () => {
-  await mongoose
-    .connect(process.env.MONGO_URI as string)
+  return mongoose
+    .connect(env.MONGO_URI)
     .then(() => console.log("MongoDB succesfully connected"))
     .catch((err) => console.log("MongoDB connection failed", err));
 };
