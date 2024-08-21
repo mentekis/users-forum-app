3fd28dcd833bcccd08f413ac9103e5b8591a348a
Dadit <dadit.utama@gmail.com>
2024-08-21 10:02:03 +0700

    Input validation (zod): adding user zodSchema then implemented in services and controllers

---
+++ src/utils/zod/user.zod.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const userValidationSchema = z.object({
+  name: z
+    .string({
+      required_error: "Name is required",
+      invalid_type_error: "Name must be a string",
+    })
+    .toUpperCase()
+    .min(1, "Name is required, fill it please"),
+  city: z
+    .string({
+      required_error: "City is required",
+      invalid_type_error: "City must be a string",
+    })
+    .toUpperCase()
+    .min(1, "Name is required, fill it please"),
+});
