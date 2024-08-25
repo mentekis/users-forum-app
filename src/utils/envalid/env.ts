bfff2a47a08e99ca1d0ec14f997eea014bea5c7a
Dadit <dadit.utama@gmail.com>
2024-08-25 08:21:50 +0700

    Env(fix): adding testOnly devDefault for jest test

--- src/utils/envalid/env.ts
+++ src/utils/envalid/env.ts
@@ -1,13 +1,13 @@
-import { cleanEnv, str } from "envalid";
+import { cleanEnv, str, testOnly } from "envalid";
 
 export const env = cleanEnv(process.env, {
-  PORT: str(),
-  MONGO_URI: str(),
-  MONGO_INITDB_ROOT_USERNAME: str(),
-  MONGO_INITDB_ROOT_PASSWORD: str(),
-  JWT_ACCESS_KEY: str(),
-  JWT_REFRESH_KEY: str(),
-  RABBITMQ_URI: str(),
-  QUEUE_NEW_USER: str(),
-  QUEUE_GET_USER: str(),
+  PORT: str({ devDefault: testOnly("3001") }),
+  MONGO_URI: str({ devDefault: testOnly("http://mongodb") }),
+  MONGO_INITDB_ROOT_USERNAME: str({ devDefault: testOnly("myusername") }),
+  MONGO_INITDB_ROOT_PASSWORD: str({ devDefault: testOnly("mypass") }),
+  JWT_ACCESS_KEY: str({ devDefault: testOnly("myaccesskey") }),
+  JWT_REFRESH_KEY: str({ devDefault: testOnly("myrefreshkey") }),
+  RABBITMQ_URI: str({ devDefault: testOnly("http://rabbitmq") }),
+  QUEUE_NEW_USER: str({ devDefault: testOnly("newUser") }),
+  QUEUE_GET_USER: str({ devDefault: testOnly("getUser") }),
 });
