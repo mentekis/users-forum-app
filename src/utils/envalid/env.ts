7e75f2d691bc3bd47d5e94b326273997751f75c6
Dadit <dadit.utama@gmail.com>
2024-08-25 20:56:14 +0700

    Env(fix): delete all devDefault solve the app issue

--- src/utils/envalid/env.ts
+++ src/utils/envalid/env.ts
@@ -10,16 +10,4 @@ export const env = cleanEnv(process.env, {
   RABBITMQ_URI: str(),
   QUEUE_NEW_USER: str(),
   QUEUE_GET_USER: str(),
-
-  // PORT: str({ devDefault: testOnly("3001") }),
-  // MONGO_URI: str({
-  //   devDefault: testOnly("mongodb://users-db:27017/users_dev"),
-  // }),
-  // MONGO_INITDB_ROOT_USERNAME: str({ devDefault: testOnly("forum_users") }),
-  // MONGO_INITDB_ROOT_PASSWORD: str({ devDefault: testOnly("forumAppUsers001") }),
-  // JWT_ACCESS_KEY: str({ devDefault: testOnly("myaccesskey") }),
-  // JWT_REFRESH_KEY: str({ devDefault: testOnly("myrefreshkey") }),
-  // RABBITMQ_URI: str({ devDefault: testOnly("http://rabbitmq") }),
-  // QUEUE_NEW_USER: str({ devDefault: testOnly("newUser") }),
-  // QUEUE_GET_USER: str({ devDefault: testOnly("getUser") }),
 });
