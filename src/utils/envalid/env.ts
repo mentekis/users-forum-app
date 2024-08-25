7fe491fc2ccdcb9d6f4ac72bc50e9d543fb7deae
Dadit <dadit.utama@gmail.com>
2024-08-25 20:47:33 +0700

    Database(fix): delete return & await keyword on db connect

--- src/utils/envalid/env.ts
+++ src/utils/envalid/env.ts
@@ -1,13 +1,25 @@
-import { cleanEnv, str, testOnly } from "envalid";
+import { cleanEnv, str } from "envalid";
 
 export const env = cleanEnv(process.env, {
-  PORT: str({ devDefault: testOnly("3001") }),
-  MONGO_URI: str({ devDefault: testOnly("http://mongodb") }),
-  MONGO_INITDB_ROOT_USERNAME: str({ devDefault: testOnly("myusername") }),
-  MONGO_INITDB_ROOT_PASSWORD: str({ devDefault: testOnly("mypass") }),
-  JWT_ACCESS_KEY: str({ devDefault: testOnly("myaccesskey") }),
-  JWT_REFRESH_KEY: str({ devDefault: testOnly("myrefreshkey") }),
-  RABBITMQ_URI: str({ devDefault: testOnly("http://rabbitmq") }),
-  QUEUE_NEW_USER: str({ devDefault: testOnly("newUser") }),
-  QUEUE_GET_USER: str({ devDefault: testOnly("getUser") }),
+  PORT: str(),
+  MONGO_URI: str(),
+  MONGO_INITDB_ROOT_USERNAME: str(),
+  MONGO_INITDB_ROOT_PASSWORD: str(),
+  JWT_ACCESS_KEY: str(),
+  JWT_REFRESH_KEY: str(),
+  RABBITMQ_URI: str(),
+  QUEUE_NEW_USER: str(),
+  QUEUE_GET_USER: str(),
+
+  // PORT: str({ devDefault: testOnly("3001") }),
+  // MONGO_URI: str({
+  //   devDefault: testOnly("mongodb://users-db:27017/users_dev"),
+  // }),
+  // MONGO_INITDB_ROOT_USERNAME: str({ devDefault: testOnly("forum_users") }),
+  // MONGO_INITDB_ROOT_PASSWORD: str({ devDefault: testOnly("forumAppUsers001") }),
+  // JWT_ACCESS_KEY: str({ devDefault: testOnly("myaccesskey") }),
+  // JWT_REFRESH_KEY: str({ devDefault: testOnly("myrefreshkey") }),
+  // RABBITMQ_URI: str({ devDefault: testOnly("http://rabbitmq") }),
+  // QUEUE_NEW_USER: str({ devDefault: testOnly("newUser") }),
+  // QUEUE_GET_USER: str({ devDefault: testOnly("getUser") }),
 });
