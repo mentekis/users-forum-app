4d9c000f30aeb94f8b95b9add46da6c4cbb661b5
Dadit <dadit.utama@gmail.com>
2024-08-28 14:07:00 +0700

    Cors (feat): adding cors at app

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import { env } from "./utils/envalid/env";
 import { userRouter } from "./routes/user.route";
 import { connectDb } from "./utils/db-connect/db.connect";
 import { newDataSuggestion } from "./utils/rabbitmq/user.rabbitmq";
+import cors from "cors";
 
 const app = express();
 connectDb();
 
 app.use(express.json());
 app.use("/api/v1/users", userRouter);
+app.use(cors({ origin: `http://${env.HOST}:3000` }));
 
 // Queue Listener
 newDataSuggestion(env.QUEUE_NEW_USER, "User");
